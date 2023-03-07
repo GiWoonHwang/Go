461d8dadffbd0227f7c8cd89ee2442b3044f94bd
jangdeukchun <dustin@Dustinui-MacBookPro.local>
2023-03-07 11:30:30 +0900

    go는 복수의 값을 리턴 가능

--- 함수.go
+++ 함수.go
@@ -10,6 +10,7 @@ package main
 func main1() {
 	msg := "Hello"
 	say1(msg)
+
 }
 
 func say1(msg string) {
@@ -27,12 +28,83 @@ Pass By Reference
 */
 
 func main() {
-	msg := "Hello"
-	say(&msg)
-	println(msg) //변경된 메시지 출력
+	// msg := "Hello"
+	// say(&msg)
+	// println(msg) //변경된 메시지 출력
+	main2()
+
 }
 
 func say(msg *string) {
 	println(*msg)
 	*msg = "Changed" //메시지 변경
+}
+
+/*
+가변인자 함수
+함수에 고정된 수의 파라미터들을 전달하지 않고 다양한 숫자의 파라미터를 전달하고자 할 때 가변 파라미터를 나타내는 ... (3개의 마침표)을 사용한다. 즉 문자열 가변 파라미터를 나타내기 위해서 ...string 과 같이 표현한다.
+가변 파라미터를 갖는 함수를 호출할 때는 0개, 1개, 2개, 혹은 n개의 동일타입 파라미터를 전달할 수 있다. 이렇게 가변 파라미터를 받아들이는 함수를 Variadic Function (가변인자함수)라고 부른다.
+아래 예제는 say 함수에 4개의 문자열을 전달할 수도 있고, 1개의 문자열을 전달할 수도 있음을 예시하고 있다
+*/
+
+func main2() {
+	say2("This", "is", "a", "book")
+	say2("Hi")
+}
+
+func say2(msg ...string) {
+	for _, s := range msg {
+		println(s)
+	}
+}
+
+/*
+Go 프로그래밍 언어에서 함수는 리턴값이 없을 수도, 리턴값이 하나 일 수도, 또는 리턴값이 복수 개일 수도 있다. C 언어에서 void 혹은 하나의 값만을 리턴하는 것과 대조적으로 Go 언어는 복수개의 값을 리턴할 수 있다.
+Go 언어는 또한 Named Return Parameter 라는 기능을 제공하는데, 이는 리턴되는 값들을 (함수에 정의된) 리턴 파라미터들에 할당할 수 있는 기능이다.
+
+함수에서 리턴값이 있는 경우는 func 문의 (파라미터 괄호 다음) 마지막에 리턴값의 타입을 정의해 준다. 그리고 값을 리턴하기 위해 함수내에서 return 키워드를 사용한다. 예를 들어, 아래 예제는 sum() 함수의 리턴 타입이 int 임을 표시하고 있으며, sum 함수 마지막에 return s 와 같이 정수 s의 값을 리턴하고 있다.
+*/
+
+func main3() {
+	total := sum(1, 7, 3, 5, 9)
+	println(total)
+}
+
+func sum(nums ...int) int {
+	s := 0
+	for _, n := range nums {
+		s += n
+	}
+	return s
+}
+
+// 복수 개의 값을 리턴하기 위해서는 해당 리턴 타입들을 괄호() 안에 적어준다.
+func main4() {
+	count, total := sum2(1, 7, 3, 5, 9)
+	println(count, total)
+}
+
+func sum2(nums ...int) (int, int) {
+	s := 0     // 합계
+	count := 0 // 요소 갯수
+	for _, n := range nums {
+		s += n
+		count++
+	}
+	return count, s
+}
+
+/*
+Go에서 Named Return Parameter들에 리턴값들을 할당하여 리턴할 수 있는데, 이는 리턴되는 값들이 여러 개일 때, 코드 가독성을 높이는 장점이 있다. 예를 들어,
+위의 sum() 함수를 Named Return Parameter를 이용하여 고쳐쓰면 다음과 같다. 아래 예제에서 func 라인의 마지막 리턴타입 정의 부분을 보면, (int, int) 가 아니라 (count int, total int) 처럼 정의되어 있음을 볼 수 있다.
+즉, 리턴 파라미터명과 그 타입을 함께 정의한 것이다. 그리고 함수 내에서는 이 count, total에 결과값을 직접 할당하고 있음을 볼 수 있다. 또한 마지막에 return 문이 있는 것을 볼 수 있는데,
+실제 return 문에는 아무 값들을 리턴하지 않지만, 그래도 리턴되는 값이 있을 경우에는 빈 return 문을 반드시 써 주어야 한다 (이를 생략하면 에러 발생).
+*/
+
+func sum3(nums ...int) (count int, total int) {
+	for _, n := range nums {
+		total += n
+	}
+	count = len(nums)
+	return
 }
